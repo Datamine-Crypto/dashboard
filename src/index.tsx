99fc3914916d551a7d6158000332fb3f8291a8e5
hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2025-01-05 22:29:39 -0500

    Added latest service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<App />
 )
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.register();
+serviceWorkerRegistration.register();
