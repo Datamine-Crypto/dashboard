c5796fd5c8fde2bf49688994ea85fdc12970840d
hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2025-05-28 21:47:16 -0400

    Upgraded to vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import { ThemeProvider } from '@mui/material';
-import { Web3ContextProvider } from './core/web3/Web3Context';
-
+import CircularProgress from '@mui/material/CircularProgress';
 import CssBaseline from '@mui/material/CssBaseline';
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import ErrorBoundary from './core/react/ErrorBoundary';
-import PageFragment from './core/react/pages/PageFragment';
+import { Web3ContextProvider } from './core/web3/Web3Context';
+const PageFragment = lazy(() => import('./core/react/pages/PageFragment'))
+
 import { theme } from './core/styles';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
+
+const CenteredLoading = () => (
+	<div
+		style={{
+			display: 'flex',
+			justifyContent: 'center',
+			alignItems: 'center',
+			height: '100vh', // Full viewport height
+			width: '100vw', // Full viewport width
+			backgroundColor: '#222', // Optional: A dark background to make white text visible
+		}}
+	>
+		<CircularProgress style={{ color: '#0ff' }} />
+	</div>
+);
 
 function App() {
 
@@ -19,9 +35,11 @@ function App() {
 			<CssBaseline />
 			<ThemeProvider theme={theme.muiTheme}>
 				<ErrorBoundary>
-					<Web3ContextProvider>
-						<PageFragment />
-					</Web3ContextProvider>
+					<Suspense fallback={<CenteredLoading />}>
+						<Web3ContextProvider>
+							<PageFragment />
+						</Web3ContextProvider>
+					</Suspense>
 				</ErrorBoundary>
 			</ThemeProvider>
 		</React.Fragment>
---
+++ src/vite-env.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-svgr/client" />
+
+// You can also add other global type declarations here if needed
+// For example, if you were referencing process.env extensively:
+// interface ImportMetaEnv {
+//   readonly VITE_APP_TITLE: string
+//   // more env variables...
+// }
+
+// interface ImportMeta {
+//   readonly env: ImportMetaEnv
+// }
