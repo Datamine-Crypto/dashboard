8b563e43042d8489b1e635d0e45a484322a96486
hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2025-06-02 10:31:46 -0400

    Removed @mui/styles

--- src/core/react/elements/LightTooltip.tsx
+++ src/core/react/elements/LightTooltip.tsx
@@ -1,21 +1,27 @@
-import { Theme } from '@mui/material/styles';
-import Tooltip from '@mui/material/Tooltip';
-import { withStyles } from '@mui/styles';
+import { styled } from '@mui/material/styles';
+import Tooltip, { TooltipProps, tooltipClasses } from '@mui/material/Tooltip';
 
+// Define the props for your custom tooltip by extending the base TooltipProps
+interface CustomTooltipProps extends TooltipProps { }
 
-const LightTooltip = withStyles((theme: Theme) => {
-	console.log('theme:', theme)
-	return {
-		tooltip: {
-			backgroundColor: '#272936',
-			color: '#fff',
-			boxShadow: '0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12)', // theme.shadows[8] was giving errors in mui v7 upgrade
-			padding: 16, // theme.spacing(2) was giving errors in mui v7 upgrade
-			fontSize: 14,
-			border: '1px solid #0ff'
-		},
-	}
-}
-)(Tooltip);
-
+// Create the styled Tooltip component
+const LightTooltip = styled(({ className, ...props }: CustomTooltipProps) => (
+	<Tooltip {...props} classes={{ popper: className }} />
+))(({ theme }) => ({
+	[`& .${tooltipClasses.tooltip}`]: {
+		backgroundColor: '#272936',
+		color: '#fff',
+		boxShadow: '0px 5px 5px -3px rgba(0,0,0,0.2),0px 8px 10px 1px rgba(0,0,0,0.14),0px 3px 14px 2px rgba(0,0,0,0.12)',
+		padding: 16, // Equivalent to theme.spacing(2) if your base spacing unit is 8px
+		fontSize: 14,
+		border: '1px solid #0ff',
+	},
+	// If you also want to style the arrow, you can add it here
+	[`& .${tooltipClasses.arrow}`]: {
+		color: '#272936', // Match the tooltip background
+		'&:before': {
+			border: '1px solid #0ff', // Match the tooltip border
+		}
+	},
+}));
 export default LightTooltip
