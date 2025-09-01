cc996d6d6cb2b5b7b507a7c900037c343624762e
hodlforjesus <50343356+hodlforjesus@users.noreply.github.com>
2025-09-01 18:10:21 -0400

    Finished eslint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -36,17 +36,13 @@ export default tseslint.config(
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     rules: {
-        "@typescript-eslint/no-explicit-any": "off", // ~197 "any" usages
-        "@typescript-eslint/no-unused-vars": "off", // ~162 unused vars (ex: catch)
-        
         "@typescript-eslint/no-empty-object-type": "off", // 13 errors, this can be an easy win 
         "react/display-name": "off", // 42 errors becauase of anonymous FC names. Can be an easy win
 
+        "@typescript-eslint/no-explicit-any": "off", // ~197 "any" usages
+        "@typescript-eslint/no-unused-vars": "off", // ~162 unused vars (ex: catch)
+        
         "react/prop-types": "off", // 497 errors, this seems like false positives? Need to investigate more
-        
-        //"no-case-declarations": "off",
-        
-        "jsx-a11y/alt-text": "off",
     },
   },
 
